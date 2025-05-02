c8d83a7a4e2b7ebc9f0a6691565ba8a0b53aa56c
sneyderbg <sneyder.buitrago@udea.edu.co>
2025-05-02 15:05:04 -0500

    se añadió el test para Solicitud de prestamo

--- src/test/scala/parabank/Data.scala
+++ src/test/scala/parabank/Data.scala
@@ -1,19 +1,22 @@
 package parabank
 
-object Data{
-    val url= "https://parabank.parasoft.com/parabank/services/bank"
-    val username = "john"
-    val password = "demo"
-    val customerId = 12989
-    val fromAccountId = 18339
-    val toAccountId = 18450
-    val ammountTransfer = 1
+object Data {
+  val url = "https://parabank.parasoft.com/parabank/services/bank"
+  val username = "john"
+  val password = "demo"
+  val customerId = 12212
+  val fromAccountId = 12345
+  val toAccountId = 12456
+  val ammountTransfer = 1
 
-    val amountBill = 25
-    val name = "Factura de agua"
-    val street = "Calle 123"
-    val city = "Medellín"
-    val state = "Antioquia"
-    val zipCode = "050001"
-    val phoneNumber = "514122"
+  val amountBill = 25
+  val name = "Factura de agua"
+  val street = "Calle 123"
+  val city = "Medellín"
+  val state = "Antioquia"
+  val zipCode = "050001"
+  val phoneNumber = "514122"
+
+  val loanAmount = 20
+  val loanDownPayment = 8
 }
